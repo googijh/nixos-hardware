41d7afc760e1194d0ac0a850d9d55a5dbd98f1eb
googijh <googijh@posteo.de>
2023-09-19 14:05:56 +0200

    added support for skylake build flags

--- common/cpu/intel/skylake/localSystem-build-skylake.nix
+++ common/cpu/intel/skylake/localSystem-build-skylake.nix
@@ -1,6 +1,11 @@
 { config, pkgs, lib, ... }:
 
 # taken from https://nixos.wiki/wiki/Build_flags
+# vendor_id       : GenuineIntel
+# cpu family      : 6
+# model           : 94
+# model name      : Intel(R) Core(TM) i7-6820HQ CPU @ 2.70GHz
+
 {
     nixpkgs.localSystem = {
       gcc.arch = "skylake";
